35750b5e43a4c69557ebe64b24b4f031be130c6c
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-06-17 18:12:59 -0400

    Initial Commit

---
+++ ErrorHandlingSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ErrorHandlingSwift
+//
+//  Created by Neeraj Prasher on 2019-06-17.
+//  Copyright © 2019 lambton. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
